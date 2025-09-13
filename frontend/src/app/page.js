f54203e8a60c9721307ed28fd9aa354cf4e33ccc
Muhammad Shoaib Khan <imkhanshoaib21@gmail.com>
2025-09-13 16:52:04 +0530

    frontend initialized

---
+++ frontend/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="m-2">
+        hi this is the app right now !!!
+      </h1>
+    </div>
+  );
+}
