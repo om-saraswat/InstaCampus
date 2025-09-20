aacc4c522e4c353dbc4ad5bd1a9d1b2e773b7829
Om Saraswat <sharmaom27022004@gmail.com>
2025-09-20 13:21:33 +0530

    some fixes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001", 
+  baseURL: pprocess.env.NEXT_PUBLIC_API_URL, 
   withCredentials: true,
   timeout: 30000,
   headers: {
